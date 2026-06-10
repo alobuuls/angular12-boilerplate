25041d0e07fab96960498de5897161bb5581b13b
Alondra <alobuuls@gmail.com>
2026-06-10 14:09:29 -0500

    🔧 chore: Add enpoints on environments to simulate errors or delay on apis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,14 @@ export const environment = {
   endpointBaseApiPoke: 'https://pokeapi.co/api/v2',
   apiUnsplash: 'https://api.unsplash.com',
   apiKeyUnsplash: 'cFN8KHhOV-QJ7-yTY-E4NR_8AHWjkDql67vUx0GotAw',
-  endpointBaseJsonPlaceholder: 'https://jsonplaceholder.typicode.com'
+  endpointBaseJsonPlaceholder: 'https://jsonplaceholder.typicode.com',
+  exampleEndpoint400: 'https://httpbin.org/status/400',
+  exampleEndpoint404: 'https://httpbin.org/status/404',
+  exampleEndpoint401: 'https://httpbin.org/status/401',
+  exampleEndpoint403: 'https://httpbin.org/status/403',
+  exampleEndpoint500: 'https://httpbin.org/status/500',
+  exampleEndpoint502: 'https://httpbin.org/status/502',
+  exampleEndpointDelay: 'https://httpbin.org/delay/3',
 };
 
 /*
